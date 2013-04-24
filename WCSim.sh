0a6ca4d0241bbcc21abe47663b200c64baafdea2
ajperch <ajperch@a0c4e33c-23ce-467b-a9a7-cea2abf087e5>
2013-04-24 15:15:16 +0000

    Removed some unnecessary files
    git-svn-id: file:///home/ajperch/svnrep/WCSim@2 a0c4e33c-23ce-467b-a9a7-cea2abf087e5

--- WCSim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ajperch/MINOS/waterCherenkov/WCSim/
-source ../setupWCSim.sh
-make
-WCSim vectors.mac
---
+++ setupWCSim.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+source /unix/lartpc/software/root/setup.sh
+echo "Root setup complete"
+source /unix/lartpc/software/geant4/setup.sh
+echo "geant4 setup complete"
+source /unix/lartpc/software/genie/setup.sh
+echo "genie setup complete"
