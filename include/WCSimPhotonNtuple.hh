c173b3b08aa714b4446da5ae7d0da35e8aa05422
ajperch <ajperch@a0c4e33c-23ce-467b-a9a7-cea2abf087e5>
2013-05-09 12:51:09 +0000

    Actually added the WCSimPhotonNtuple.cc and .hh files
    git-svn-id: file:///home/ajperch/svnrep/WCSim@4 a0c4e33c-23ce-467b-a9a7-cea2abf087e5

---
+++ include/WCSimPhotonNtuple.hh
@@ -0,0 +1,88 @@
+#ifndef WCSimPhotonNtuple_h
+#define WCSimPhotonNtuple_h
+
+
+#include "TString.h"
+
+class TFile;
+class TTree;
+
+class WCSimPhotonNtuple
+{      
+ public :
+            
+  WCSimPhotonNtuple();         
+  ~WCSimPhotonNtuple();
+     
+  static WCSimPhotonNtuple* Instance();  
+  
+  static void Open(const char* filename);
+  static void Close();
+  static void Fill(Int_t eventID,
+                   Int_t pdgCode, Int_t trackID, Int_t parentID, Int_t processID,
+                   Double_t energy, Double_t lambda,
+                   Bool_t opticalPhoton,  Bool_t scatteredPhoton,
+                   Double_t vtxX, Double_t vtxY, Double_t vtxZ, Double_t vtxTime,
+                   Double_t endX, Double_t endY, Double_t endZ, Double_t endTime,
+                   Double_t vtxdirX, Double_t vtxdirY, Double_t vtxdirZ 
+);
+  static void FileName(const char* filename);
+
+  void SetFileName(const char* filename) { fWCFileName = filename; }
+  void OpenFile(const char* filename);
+  void CloseFile();
+
+  void WriteEvent( Int_t eventID,
+                   Int_t pdgCode, Int_t trackID, Int_t parentID, Int_t processID,
+                   Double_t energy, Double_t lambda,
+                   Bool_t opticalPhoton,  Bool_t scatteredPhoton,
+                   Double_t vtxX, Double_t vtxY, Double_t vtxZ, Double_t vtxTime,
+                   Double_t endX, Double_t endY, Double_t endZ, Double_t endTime,
+                   Double_t vtxdirX, Double_t vtxdirY, Double_t vtxdirZ );
+
+  void SetGeometry( Double_t halfWidthXY, Double_t halfWidthZ ) {
+    fHalfWidthXY = halfWidthXY; fHalfWidthZ = halfWidthZ;
+  }
+
+ private :
+    
+  void WriteEventToFile();
+  void WriteToFile();
+  
+
+  TFile* fWCFile;
+  TTree* fWCTree;
+  TString fWCFileName;
+
+  Int_t fEventID;
+  Int_t fPdgCode;
+  Int_t fTrackID;
+  Int_t fParentID;
+  Int_t fProcessID;
+  Double_t fEnergy; 
+  Double_t fLambda;
+  Int_t fOpticalPhoton;
+  Int_t fScatteredPhoton;
+  Double_t fVtxX;
+  Double_t fVtxY;
+  Double_t fVtxZ;
+  Double_t fVtxTime;
+  Double_t fVtxDirX; 
+  Double_t fVtxDirY; 
+  Double_t fVtxDirZ;  
+  Double_t fEndX;
+  Double_t fEndY;
+  Double_t fEndZ;
+  Double_t fEndTime;
+  
+  Int_t fIsDetected;
+  Double_t fEndU;
+  Double_t fEndV;
+
+  Double_t fHalfWidthXY;
+  Double_t fHalfWidthZ;
+         
+} ;
+
+
+#endif
---
+++ src/WCSimPhotonNtuple.cc
@@ -0,0 +1,169 @@
+#include "WCSimPhotonNtuple.hh"
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TDirectory.h"
+
+#include <iostream>
+
+static WCSimPhotonNtuple* fgNtuple = 0;
+
+WCSimPhotonNtuple* WCSimPhotonNtuple::Instance()
+{
+  if( !fgNtuple ){
+    fgNtuple = new WCSimPhotonNtuple();
+  }
+
+  return fgNtuple;
+}
+
+WCSimPhotonNtuple::WCSimPhotonNtuple() 
+{
+  fWCFile = 0;
+  fWCTree = 0;  
+  fWCFileName = "/home/ajperch/asdfghjk.root";
+
+  fHalfWidthZ = 0.0;
+  fHalfWidthXY = 0.0;
+}
+
+WCSimPhotonNtuple::~WCSimPhotonNtuple()
+{
+  WCSimPhotonNtuple::Instance()->CloseFile();
+}
+
+void WCSimPhotonNtuple::Open(const char* filename)
+{
+  WCSimPhotonNtuple::Instance()->OpenFile(filename);
+}
+
+void WCSimPhotonNtuple::Close()
+{
+  WCSimPhotonNtuple::Instance()->CloseFile();
+}
+
+void WCSimPhotonNtuple::FileName(const char* filename)
+{
+  WCSimPhotonNtuple::Instance()->SetFileName(filename);
+}
+
+void WCSimPhotonNtuple::Fill( Int_t eventID, Int_t pdgCode, Int_t trackID, Int_t parentID, Int_t processID, Double_t energy, Double_t lambda, Bool_t opticalPhoton,  Bool_t scatteredPhoton, Double_t vtxX, Double_t vtxY, Double_t vtxZ, Double_t vtxTime, Double_t endX, Double_t endY, Double_t endZ, Double_t endTime, Double_t vtxdirX, Double_t vtxdirY, Double_t vtxdirZ ) 
+{
+  WCSimPhotonNtuple::Instance()->WriteEvent(eventID, pdgCode, trackID, parentID, processID, 
+                                         energy, lambda, 
+                                         opticalPhoton, scatteredPhoton,
+                                         vtxX, vtxY, vtxZ, vtxTime,
+                                         endX, endY, endZ, endTime,
+				                             vtxdirX, vtxdirY, vtxdirZ );
+}
+
+void WCSimPhotonNtuple::WriteEvent( Int_t eventID, Int_t pdgCode, Int_t trackID, Int_t parentID, Int_t processID, Double_t energy, Double_t lambda, Bool_t opticalPhoton,  Bool_t scatteredPhoton, Double_t vtxX, Double_t vtxY, Double_t vtxZ, Double_t vtxTime, Double_t endX, Double_t endY, Double_t endZ, Double_t endTime, Double_t vtxdirX, Double_t vtxdirY, Double_t vtxdirZ ) 
+{
+  fEventID = eventID;
+  fPdgCode = pdgCode;
+  fTrackID = trackID;
+  fParentID = parentID;
+  fProcessID = processID;
+  fEnergy = energy;
+  fLambda = lambda;
+  fOpticalPhoton = opticalPhoton;
+  fScatteredPhoton = scatteredPhoton;
+  fVtxX = vtxX;
+  fVtxY = vtxY;
+  fVtxZ = vtxZ;
+  fVtxTime = vtxTime;
+  fEndX = endX;
+  fEndY = endY;
+  fEndZ = endZ;
+  fEndTime = endTime;
+
+  fVtxDirX = vtxdirX;
+  fVtxDirY = vtxdirY;
+  fVtxDirZ = vtxdirZ;
+  
+  fIsDetected = 0;
+
+  if( fOpticalPhoton && fParentID==1 && fProcessID == 2 ){ // Added the || 1 - AJP
+    this->WriteEventToFile();
+  }
+}
+
+void WCSimPhotonNtuple::WriteEventToFile()
+{
+   
+  this->OpenFile(fWCFileName.Data());
+
+  this->WriteToFile();
+
+  return;
+}
+
+void WCSimPhotonNtuple::OpenFile(const char* filename)
+{
+  TDirectory* tmpd = 0;
+
+  if( fWCFile==0 ){
+    tmpd = gDirectory;
+    std::cout << " opening file: " << filename << std::endl;
+    fWCFile = new TFile(filename,"recreate");
+    fWCTree = new TTree("ntuple","my analysis ntuple");
+    fWCTree->Branch("eventID",&fEventID,"eventID/I");
+    fWCTree->Branch("pdgCode",&fPdgCode,"pdgCode/I");
+    fWCTree->Branch("trackID",&fTrackID,"trackID/I");
+    fWCTree->Branch("parentID",&fParentID,"parentID/I");
+    fWCTree->Branch("processID",&fProcessID,"processID/I");
+    fWCTree->Branch("energy",&fEnergy,"energy/D");
+    fWCTree->Branch("wavelength",&fLambda,"wavelength/D");
+    fWCTree->Branch("optical",&fOpticalPhoton,"optical/I");
+    fWCTree->Branch("scattered",&fScatteredPhoton,"scattered/I");
+    fWCTree->Branch("vtxX",&fVtxX,"vtxX/D");
+    fWCTree->Branch("vtxY",&fVtxY,"vtxY/D");
+    fWCTree->Branch("vtxZ",&fVtxZ,"vtxZ/D");
+    //fWCTree->Branch("vtxTime",&fVtxTime,"vtxTime/D");
+    fWCTree->Branch("endX",&fEndX,"endX/D");
+    fWCTree->Branch("endY",&fEndY,"endY/D");
+    fWCTree->Branch("endZ",&fEndZ,"endZ/D");
+//    fWCTree->Branch("endU",&fEndU,"endU/D");
+//    fWCTree->Branch("endV",&fEndV,"endV/D");
+//    //fWCTree->Branch("endTime",&fEndTime,"endTime/D");
+    fWCTree->Branch("vtxdirX",&fVtxDirX,"vtxdirX/D");
+    fWCTree->Branch("vtxdirY",&fVtxDirY,"vtxdirY/D");
+    fWCTree->Branch("vtxdirZ",&fVtxDirZ,"vtxdirZ/D");
+    //fWCTree->Branch("isDetected",&fIsDetected,"isDetected/I");
+    gDirectory = tmpd;
+  }
+
+  return;
+}
+
+void WCSimPhotonNtuple::WriteToFile()
+{
+  TDirectory* tmpd = 0;
+
+  if( fWCFile ){
+    tmpd = gDirectory;
+    fWCFile->cd();
+    fWCTree->Fill();
+    gDirectory = tmpd;
+  }
+
+  return;
+}
+
+void WCSimPhotonNtuple::CloseFile()
+{
+  TDirectory* tmpd = 0;
+
+  if( fWCFile ){
+    tmpd = gDirectory;
+    std::cout << " closing file: " << fWCFile->GetName() << std::endl;
+    fWCFile->cd();
+    fWCTree->Write();
+    fWCFile->Close();
+    fWCTree = 0;
+    fWCFile = 0;
+    gDirectory = tmpd;
+  }
+
+  return;
+}
