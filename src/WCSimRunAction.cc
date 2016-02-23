2b29a944e428e551b2ed7ba7f2dc6016d52d3722
Andy Perch <a.perch.12@ucl.ac.uk>
2016-02-23 09:56:52 +0000

    Made the run action print a message at the end of the run reminding you where the output file was saved

--- src/WCSimRunAction.cc
+++ src/WCSimRunAction.cc
@@ -110,12 +110,16 @@ void WCSimRunAction::EndOfRunAction(const G4Run*)
 //        << "% through-going (hit Catcher)" << G4endl;
 
   // Close the Root file at the end of the run
+  int events = WCSimTree->GetEntries();
   if( GetSavePhotonNtuple() ) { WCSimPhotonNtuple::Close(); }
   TFile* hfile = WCSimTree->GetCurrentFile();
   hfile->Close();
 
   // Clean up stuff on the heap; I think deletion of hfile and trees
   // is taken care of by the file close
+ 
+  std::cout << "Finished running and saved " << events << " events to " << GetRootFileName() << std::endl;
+
 
   delete wcsimrootsuperevent; wcsimrootsuperevent=0;
   delete wcsimrootgeom; wcsimrootgeom=0;
