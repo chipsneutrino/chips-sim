8dc4cae6d5dbbe192db8dbf6d73d526309ae7d8e
jtingey <j.tingey.16@ucl.ac.uk>
2019-10-04 16:35:21 +0100

    Updated UCL setup script

--- setupScripts/setupWCSimUCL.sh
+++ setupScripts/setupWCSimUCL.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-
-#WCSim Home Directory
 SOURCE="${BASH_SOURCE[0]}"
 # While ${SOURCE} is a symbolic link, resolve it
 while [ -h "${SOURCE}" ]; do
@@ -12,9 +10,9 @@ done
 DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd | sed "s#setupScripts##g")"
 export WCSIMHOME=${DIR}
 
+
+
 source /unix/lartpc/software/root/setup.sh
-echo "Root setup complete"
 source /unix/lartpc/software/geant4/setup.sh
-echo "geant4 setup complete"
 source /unix/lartpc/software/genie/setup.sh
-echo "genie setup complete"
+echo "WCSim setup complete"
